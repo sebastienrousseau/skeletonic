355d3a7fc96913668f1073075de39a25564dbced
Sebastien Rousseau <sebastien.rousseau@mac.com>
2017-12-20 17:41:40 +0000

    Initial commit v0.0.1
    * Added source files inspired by http://usewing.ml/
    * Added CSS files
    * Configuring gulpfile.js, package.json
    * Updated README and CONTRIBUTING

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# skeletonic
-💀 Skeletonic is a dead on, lightweight, responsive and modern CSS framework
+<p align="center"><a href="https://skeletonic.io" target="_blank"><img width="125"src="https://github.com/reedia/skeletonic/raw/gh-pages/images/logo.png"></a></p>
+
+<h1 align="center">Skeletonic</h1>
+
+<p align="center">💀 Skeletonic is a dead on, lightweight, responsive and modern CSS framework</p>
+
+[![Build Status](https://travis-ci.org/reedia/skeletonic.svg?branch=master)](https://travis-ci.org/reedia/skeletonic)
+[![Packagist](https://img.shields.io/badge/license-MIT-blue.svg)](https://kbrsh.github.io/license)
+
+## Why should you use it?
+
+Everything you need to create a basic project is included, all in a **2kb** file (minified + gzipped). On top of that, almost everything is styled automatically, without the need for learning specific classes. This allows for an intuitive experience developing with Wing, as it's designed to be a boilerplate stylesheet for any project.
+
+## Installation
+
+**npm**
+
+```sh
+$ npm install --save skeletonic
+```
+
+**Link to It**
+
+```html
+<link rel="stylesheet" href="https://unpkg.com/skeletonic"/>
+```
+
+## [Documentation](https://skeletonic.io/)
+
+## Find a bug?
+
+Submit it in the [issues](https://github.com/reedia/skeletonic/issues)
+
+## License
+
+Licensed under the [MIT License](http://kbrsh.github.io/license) by [Kabir Shah](http://kabir.ml)
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require("gulp");
+const pkg = require("./package.json");
+
+const minifyCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const header = require("gulp-header");
+const size = require("gulp-size");
+const stylus = require("gulp-stylus");
+const csslint = require("gulp-csslint");
+const csscomb = require('gulp-csscomb');
+
+const comment = `/**
+ * Skeletonic v${pkg.version}
+ * Copyright © Reedia Limited 2015-2018. All rights reserved.
+ * https://skeletonic.io/
+ * Released under the MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ */\r\n`;
+
+gulp.task("build", function () {
+  return gulp.src(["./src/config.styl", "./src/base.styl", "./src/grid.styl", "./src/typography.styl", "./src/form.styl", "./src/button.styl", "./src/link.styl", "./src/list.styl", "./src/image.styl", "./src/box.styl", "./src/nav.styl", "./src/card.styl", "./src/code.styl", "./src/divider.styl", "./src/util.styl"])
+    .pipe(concat("skeletonic.styl"))
+    .pipe(stylus())
+    .pipe(header(comment + "\r\n"))
+    .pipe(size())
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("csslint", ["build"], function() {
+  return gulp.src(["./dist/skeletonic.css"])
+    .pipe(csslint())
+    .pipe(csslint.formatter())
+    .pipe(header(comment))
+    .pipe(size())
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task('csscomb', function() {
+  return gulp.src(["./dist/skeletonic.css"])
+    .pipe(csscomb('./csscomb.json'))
+    .pipe(header(comment + "\r\n"))
+    .pipe(size())
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("minify", ["build"], function() {
+  return gulp.src(["./dist/skeletonic.css"])
+    .pipe(minifyCSS())
+    .pipe(header(comment))
+    .pipe(size())
+    .pipe(size({
+      gzip: true
+    }))
+    .pipe(concat("skeletonic.min.css"))
+    .pipe(gulp.dest("./dist/"));
+});
+
+
+gulp.task("watch", function() {
+  gulp.watch(["src/*.css"], ["default"]);
+});
+
+
+gulp.task("default", ["build","csscomb","csslint","minify"]);
